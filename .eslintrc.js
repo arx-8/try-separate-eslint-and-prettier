f1f6ff803d6fefd2223cac384d284d9694807fbd
arx-8 <swans.randomness@gmail.com>
2020-10-18 17:36:45 +0900

    Sort all

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,14 @@ module.exports = {
   plugins: [
     // CI で warn も検知可能にするため、全て error にする
     "only-error",
+    "sort-destructure-keys",
+    "sort-keys-fix",
+    "typescript-sort-keys",
   ],
-  rules: {},
+  rules: {
+    "sort-destructure-keys/sort-destructure-keys": "error",
+    "sort-keys-fix/sort-keys-fix": "error",
+    "typescript-sort-keys/interface": "error",
+    "typescript-sort-keys/string-enum": "error",
+  },
 }
